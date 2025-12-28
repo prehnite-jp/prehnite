59931cb43b164c420174d46290f52fba46a7ee4a
saku shirakura <saku@sakushira.com>
2025-12-29 04:47:13 +0900

    [Chore] Cleaned up unnecessary files.

--- src/db/schema/schema_impl.rs
+++
@@ -1,7 +0,0 @@
-use crate::db::schema::{BibliographyAuthor, ItemType};
-
-impl Default for ItemType {
-    fn default() -> Self {
-        Self::Headline(None)
-    }
-}
--- src/db/util.rs
+++
@@ -1,3 +0,0 @@
-pub fn placeholder_helper(placeholder: impl AsRef<str>, count: usize) -> String {
-    vec![placeholder.as_ref(); count].join(",")
-}
