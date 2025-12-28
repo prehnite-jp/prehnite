1a9216490cbf0d445953fe923c6290fb174a92bb
saku shirakura <saku@sakushira.com>
2025-12-29 04:31:08 +0900

    [Feature] Implemented lazy loading queries.

--- src/db/schema/load_impl.rs
+++ src/db/schema/load_impl.rs
@@ -1,26 +1,25 @@
+use crate::db::schema::schema_binder_helper_impl::placeholder_in_clause;
 use crate::db::schema::{
     BackgroundInfo, BackgroundReference, Bibliography, BibliographyAuthor, Draft, Headline, Item,
     ItemReference, Paragraph, ParagraphSummary, Tag, Task,
 };
 use sqlx::{Error, Row, SqliteConnection};
 
-pub trait LoadAll {
-    async fn load_all(&mut self, conn: &mut SqliteConnection) -> Result<(), Error>;
-}
-
 impl BackgroundInfo {
     pub async fn load_references(&mut self, conn: &mut SqliteConnection) -> Result<(), Error> {
         self.references = Some(
-            sqlx::query_as::<_, BackgroundReference>("")
-                .bind(self.id)
-                .fetch_all(conn)
-                .await?,
+            sqlx::query_as::<_, BackgroundReference>(
+                "SELECT * FROM view_deserializable_background_reference WHERE background_info_id=?",
+            )
+            .bind(self.id)
+            .fetch_all(conn)
+            .await?,
         );
         Ok(())
     }
 }
 
-impl LoadAll for BackgroundInfo {
+impl BackgroundInfo {
     async fn load_all(&mut self, conn: &mut SqliteConnection) -> Result<(), Error> {
         self.load_references(conn).await
     }
@@ -28,15 +27,20 @@ impl LoadAll for BackgroundInfo {
 
 impl Bibliography {
     pub async fn load_authors(&mut self, conn: &mut SqliteConnection) -> Result<(), Error> {
-        self.authors = sqlx::query_as::<_, BibliographyAuthor>("")
-            .bind(self.id)
-            .fetch_all(conn)
-            .await?;
+        self.authors = sqlx::query_as::<_, BibliographyAuthor>(
+            "SELECT * FROM main.rel_bibliography_authors
+    LEFT OUTER JOIN main.bibliography_authors author
+    ON rel_bibliography_authors.bibliography_author_id = author.id
+    WHERE rel_bibliography_authors.bibliography_id=?",
+        )
+        .bind(self.id)
+        .fetch_all(conn)
+        .await?;
         Ok(())
     }
 }
 
-impl LoadAll for Bibliography {
+impl Bibliography {
     async fn load_all(&mut self, conn: &mut SqliteConnection) -> Result<(), Error> {
         self.load_authors(conn).await
     }
@@ -45,17 +49,19 @@ impl LoadAll for Bibliography {
 impl Item {
     pub async fn load_references(&mut self, conn: &mut SqliteConnection) -> Result<(), Error> {
         self.references = Some(
-            sqlx::query_as::<_, ItemReference>("SELECT * FROM item_references_list WHERE ?")
-                .bind(self.id)
-                .fetch_all(conn)
-                .await?,
+            sqlx::query_as::<_, ItemReference>(
+                "SELECT * FROM view_deserializable_item_reference WHERE item_id=?",
+            )
+            .bind(self.id)
+            .fetch_all(conn)
+            .await?,
         );
         Ok(())
     }
 
     pub async fn load_tags(&mut self, conn: &mut SqliteConnection) -> Result<(), Error> {
         self.tags = Some(
-            sqlx::query_as::<_, Tag>("SELECT * FROM item_references_list")
+            sqlx::query_as::<_, Tag>("SELECT tags.* FROM tags LEFT OUTER JOIN rel_tag_and_item ON tags.id = rel_tag_and_item.tag_id WHERE item_id=?")
                 .bind(self.id)
                 .fetch_all(conn)
                 .await?,
@@ -68,17 +74,22 @@ impl Item {
         conn: &mut SqliteConnection,
     ) -> Result<(), Error> {
         self.background_info_list = Some(
-            sqlx::query_as::<_, BackgroundInfo>("SELECT * FROM item_references_list")
-                .bind(self.id)
-                .fetch_all(conn)
-                .await?,
+            sqlx::query_as::<_, BackgroundInfo>(
+                "SELECT background_info.* FROM background_info
+    LEFT OUTER JOIN rel_background_and_item
+        ON background_info.id = rel_background_and_item.background_info_id
+    WHERE item_id=?",
+            )
+            .bind(self.id)
+            .fetch_all(conn)
+            .await?,
         );
         Ok(())
     }
 
     pub async fn load_tasks(&mut self, conn: &mut SqliteConnection) -> Result<(), Error> {
         self.tasks = Some(
-            sqlx::query_as::<_, Task>("SELECT * FROM item_references_list")
+            sqlx::query_as::<_, Task>("SELECT * FROM view_deserializable_task WHERE item_id=?")
                 .bind(self.id)
                 .fetch_all(conn)
                 .await?,
@@ -87,7 +98,7 @@ impl Item {
     }
 }
 
-impl LoadAll for Item {
+impl Item {
     async fn load_all(&mut self, conn: &mut SqliteConnection) -> Result<(), Error> {
         self.load_references(conn).await?;
         self.load_tags(conn).await?;
@@ -124,20 +135,20 @@ impl Headline {
                 .collect(),
         };
 
-        sqlx::query_as::<_, Headline>(
-            format!(
-                "SELECT * FROM headlines WHERE id IN ({})",
-                id_list.join(",")
-            )
-            .as_str(),
-        )
-        .fetch_all(conn)
-        .await
+        let sql = format!(
+            "SELECT * FROM headlines WHERE id IN ({})",
+            placeholder_in_clause(&id_list)
+        );
+        let mut query = sqlx::query_as::<_, Headline>(sql.as_str());
+        for i in id_list {
+            query = query.bind(i);
+        }
+        query.fetch_all(conn).await
     }
 
     pub async fn load_paragraph(&mut self, conn: &mut SqliteConnection) -> Result<(), Error> {
         self.paragraph = Some(
-            sqlx::query_as::<_, Paragraph>("SELECT * FROM item_references_list WHERE ?")
+            sqlx::query_as::<_, Paragraph>("SELECT * FROM item_references WHERE ?")
                 .bind(self.id)
                 .fetch_all(conn)
                 .await?,
@@ -146,7 +157,7 @@ impl Headline {
     }
 }
 
-impl LoadAll for Headline {
+impl Headline {
     async fn load_all(&mut self, conn: &mut SqliteConnection) -> Result<(), Error> {
         self.load_children(conn).await?;
         self.load_paragraph(conn).await
@@ -155,21 +166,29 @@ impl LoadAll for Headline {
 
 impl Paragraph {
     pub async fn load_draft(&mut self, conn: &mut SqliteConnection) -> Result<(), Error> {
-        self.draft = Some(sqlx::query_as::<_, Draft>("").fetch_all(conn).await?);
-        Ok(())
-    }
-
-    pub async fn load_summary(&mut self, conn: &mut SqliteConnection) -> Result<(), Error> {
-        self.summary = Some(
-            sqlx::query_as::<_, ParagraphSummary>("")
+        self.draft = Some(
+            sqlx::query_as::<_, Draft>("SELECT * FROM draft WHERE paragraph_id=?")
+                .bind(self.id)
                 .fetch_all(conn)
                 .await?,
         );
         Ok(())
     }
+
+    pub async fn load_summary(&mut self, conn: &mut SqliteConnection) -> Result<(), Error> {
+        self.summary = Some(
+            sqlx::query_as::<_, ParagraphSummary>(
+                "SELECT * FROM paragraph_summaries WHERE paragraph_id=?",
+            )
+            .bind(self.id)
+            .fetch_all(conn)
+            .await?,
+        );
+        Ok(())
+    }
 }
 
-impl LoadAll for Paragraph {
+impl Paragraph {
     async fn load_all(&mut self, conn: &mut SqliteConnection) -> Result<(), Error> {
         self.load_draft(conn).await?;
         self.load_summary(conn).await
