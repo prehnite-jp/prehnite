f1cf6c84dc7a7b11b808ccbacb7357cf8f3d2893
saku shirakura <saku@sakushira.com>
2025-12-30 00:08:45 +0900

    [Chore] cleanup

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -1,13 +1,12 @@
 #![allow(unused)]
+pub mod app_global_schema;
 pub mod error;
 pub mod schema;
-pub mod app_global_schema;
 mod util;
 
 use sqlx::migrate::Migrator;
 use sqlx::sqlx_macros::migrate;
 use sqlx::SqlitePool;
-use std::path::PathBuf;
 
 fn app_global_migrator() -> Migrator {
     migrate!("./migrations_app_global")
@@ -25,12 +24,11 @@ pub enum MigrateMode {
 pub async fn migrate(
     conn: &SqlitePool,
     mode: MigrateMode,
-    file_path: impl Into<PathBuf>,
 ) -> Result<(), sqlx::migrate::MigrateError> {
     match mode {
         MigrateMode::PrehniteBook => book_migrator(),
         MigrateMode::AppGlobal => app_global_migrator(),
     }
-        .run(conn)
-        .await
+    .run(conn)
+    .await
 }
