4e277b099d5ab8a97f056c03bd82f9317268650d
saku shirakura <saku@sakushira.com>
2026-01-05 18:37:47 +0900

    [add] add struct method acquire for database.

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -3,14 +3,27 @@ pub mod error;
 pub mod schema;
 mod util;
 
-use crate::util::fatal_init_db_error;
+use crate::db::migrate::migrate;
 use crate::fatal_init_db_error;
+use crate::util::fatal_init_db_error;
+use sqlx::pool::PoolConnection;
 use sqlx::sqlite::{SqliteConnectOptions, SqlitePoolOptions};
-use sqlx::SqlitePool;
+use sqlx::{Sqlite, SqlitePool};
 use std::ffi::OsString;
 use std::fmt::{Debug, Display, Formatter};
+use std::path::Path;
+use std::sync::Arc;
+use tokio::sync::Mutex;
+
+pub enum DBType {
+    PrehniteBook,
+    AppGlobal,
+}
 
 pub mod migrate {
+    use crate::db::DBType;
+    use sqlx::SqlitePool;
+
     pub mod prehnite_book {
         use sqlx::migrate::Migrator;
         use sqlx::sqlx_macros::migrate;
@@ -24,25 +37,41 @@ pub mod migrate {
 
         pub static MIGRATOR: Migrator = migrate!("./migrations/app_global");
     }
-}
 
-pub enum MigrateMode {
-    PrehniteBook,
-    AppGlobal,
-}
-
-async fn migrate(pool: &SqlitePool, mode: MigrateMode) -> Result<(), sqlx::migrate::MigrateError> {
-    match mode {
-        MigrateMode::PrehniteBook => &migrate::prehnite_book::MIGRATOR,
-        MigrateMode::AppGlobal => &migrate::app_global::MIGRATOR,
+    pub async fn migrate(
+        pool: &SqlitePool,
+        mode: DBType,
+    ) -> Result<(), sqlx::migrate::MigrateError> {
+        match mode {
+            DBType::PrehniteBook => &prehnite_book::MIGRATOR,
+            DBType::AppGlobal => &app_global::MIGRATOR,
+        }
+        .run(pool)
+        .await
     }
-    .run(pool)
-    .await
+}
+
+struct Pool {
+    pool: Option<SqlitePool>,
+}
+
+impl Pool {
+    fn new(pool: Option<SqlitePool>) -> Self {
+        Self { pool }
+    }
+
+    fn set_pool(&mut self, pool: Option<SqlitePool>) {
+        self.pool = pool;
+    }
+
+    fn get_pool(&self) -> &Option<SqlitePool> {
+        &self.pool
+    }
 }
 
 pub struct Database {
-    pub app_global_db_pool: SqlitePool,
-    pub prehnite_book_db_pool: Option<SqlitePool>,
+    app_global_db_pool: Arc<Mutex<Pool>>,
+    prehnite_book_db_pool: Arc<Mutex<Pool>>,
 }
 
 #[derive(Debug)]
@@ -90,29 +119,54 @@ impl Database {
         }
     }
 
+    fn connect_option(file: impl AsRef<Path>) -> SqliteConnectOptions {
+        SqliteConnectOptions::default()
+            .filename(file)
+            .foreign_keys(true)
+            .create_if_missing(true)
+    }
+
     pub async fn initialize() -> Result<Self, DatabaseError> {
         let app_global_path = std::env::var_os("APP_GLOBAL_DATABASE_PATH")
             .unwrap_or_else(Self::get_app_global_database_url);
         let mut pool = SqlitePoolOptions::new()
-            .connect_with(
-                SqliteConnectOptions::default()
-                    .filename(app_global_path)
-                    .foreign_keys(true)
-                    .create_if_missing(true),
-            )
+            .connect_with(Self::connect_option(app_global_path))
             .await?;
 
-        let mut self_res = Self {
-            app_global_db_pool: pool,
-            prehnite_book_db_pool: None,
-        };
+        migrate(&mut pool, DBType::AppGlobal).await?;
 
-        self_res.migrate().await?;
-
-        Ok(self_res)
+        Ok(Self {
+            app_global_db_pool: Arc::new(Mutex::new(Pool::new(Some(pool)))),
+            prehnite_book_db_pool: Arc::new(Mutex::new(Pool::new(None))),
+        })
     }
 
-    async fn migrate(&mut self) -> Result<(), sqlx::migrate::MigrateError> {
-        migrate(&mut self.app_global_db_pool, MigrateMode::AppGlobal).await
+    pub async fn open_book(&mut self, path: impl AsRef<Path>) -> Result<(), DatabaseError> {
+        let mut pool = SqlitePoolOptions::new()
+            .connect_with(Self::connect_option(path))
+            .await?;
+
+        migrate(&mut pool, DBType::PrehniteBook).await?;
+
+        self.prehnite_book_db_pool.lock().await.set_pool(Some(pool));
+        Ok(())
+    }
+
+    pub async fn acquire(
+        &self,
+        mode: DBType,
+    ) -> Result<Option<PoolConnection<Sqlite>>, sqlx::Error> {
+        Ok(match mode {
+            DBType::PrehniteBook => {
+                match self.prehnite_book_db_pool.clone().lock().await.get_pool() {
+                    None => None,
+                    Some(v) => Some(v.acquire().await?),
+                }
+            }
+            DBType::AppGlobal => match self.app_global_db_pool.clone().lock().await.get_pool() {
+                None => None,
+                Some(v) => Some(v.acquire().await?),
+            },
+        })
     }
 }
