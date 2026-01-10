4c4762c05ba33dedbff0cb8d2b0c577308f00e20
saku shirakura <saku@sakushira.com>
2026-01-10 17:27:17 +0900

    [Add] Introduce `opt_unwrap_or_return` macro for optional value handling

--- core/src/util/mod.rs
+++ core/src/util/mod.rs
@@ -1,3 +1,14 @@
 pub mod app_global;
 pub mod file_dialog;
 pub mod alert;
+
+#[allow(unused)]
+#[macro_export]
+macro_rules! opt_unwrap_or_return {
+    ($value:expr, $ret_val:expr) => {
+        match $value {
+            Some(v) => v,
+            None => return $ret_val
+        }
+    };
+}
