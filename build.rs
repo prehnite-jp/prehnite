e32b06211b61b5b14791c01cc2e55ec4d33129dc
saku shirakura <saku@sakushira.com>
2026-02-27 15:16:51 +0900

    [chore] Add rerun-if-changed directives to build script for improved rebuild management.

--- build.rs
+++ build.rs
@@ -3,6 +3,12 @@
 use prehnite_build_libs::util::set_env;
 
 fn main() {
+    println!("cargo::rerun-if-changed=build.rs");
+    println!("cargo::rerun-if-changed=build-libs");
+    println!("cargo::rerun-if-changed=license-bundle");
+    println!("cargo::rerun-if-changed=font-manager");
+    println!("cargo::rerun-if-changed=Cargo.lock");
+    println!("cargo::rerun-if-changed=Cargo.toml");
     set_env(
         "BUILD_INFO_TARGET",
         std::env::var("TARGET").unwrap().as_str(),
