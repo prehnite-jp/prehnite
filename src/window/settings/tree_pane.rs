c24c3e9fb8eb361c1fad0e57342bf4ab9d15da6f
saku shirakura <saku@sakushira.com>
2026-05-02 13:01:44 +0900

    [feature] add settings tree pane

--- src/window/settings/tree_pane.rs
+++ src/window/settings/tree_pane.rs
@@ -1,6 +1,113 @@
+use crate::app::settings::setting_schema::GlobalSettings;
+use crate::components::accordion::{AccordionContent, AccordionItem};
+use crate::components::scroll_area::ScrollArea;
+use crate::custom_dx_components::accordion::{CustomAccordion, CustomAccordionTrigger};
+use crate::window::settings::{visible_children, visible_filter};
 use dioxus::prelude::*;
+use dioxus_i18n::t;
+use dioxus_primitives::scroll_area::ScrollDirection;
+use easy_settings::{Registry, RegistryNode};
+
+#[component]
+fn SettingEntry(node: &'static str) -> Element {
+    rsx!{
+        span {
+            text_wrap: "nowrap",
+            margin_right: "2em",
+            font_size: "11px",
+            { t!(&format!("settings_entry_{}", node)) }
+        }
+    }
+}
+
+#[component]
+fn ItemNode(node: &'static str) -> Element {
+    rsx! {
+        AccordionContent {
+            SettingEntry {
+                node
+            }
+        }
+    }
+}
+
+#[component]
+fn Nodes(children_node: &'static [RegistryNode]) -> Element {
+    let children_node = children_node.iter().filter(visible_filter);
+    rsx! {
+        for (index, category) in children_node.clone().filter_map(|x| if x.is_category() { Some(x.value()) } else { None }).enumerate() {
+            AccordionContent {
+                CustomAccordion {
+                    CategoryTree {
+                        index,
+                        category
+                    }
+                }
+            }
+        }
+        for node in children_node.filter_map(|x| if x.is_setting_item() { Some(x.value()) } else { None }) {
+            ItemNode {
+                node
+            }
+        }
+    }
+}
+
+#[component]
+fn CategoryTree(index: usize, category: &'static str) -> Element {
+    let children_node = GlobalSettings::child_nodes(Some(category));
+    rsx! {
+        AccordionItem {
+            index,
+            CustomAccordionTrigger {
+                span {
+                    { t!(&format!("settings_category_{}", category)) }
+                }
+            },
+            Nodes {
+                children_node
+            }
+        }
+    }
+}
 
 #[component]
 pub fn SettingTreePane() -> Element {
-    rsx! {}
+    let children_node = visible_children(None);
+
+    rsx! {
+        div {
+            display: "flex",
+            flex_direction: "column",
+            div {
+                border_bottom: "thin solid",
+                border_color: "var(--secondary-color-5)",
+                p {
+                    text_overflow: "ellipsis",
+                    margin: "0.5em 1em",
+                    white_space: "pre",
+                    " "
+                }
+            }
+            ScrollArea {
+                height: "calc(100vh - 7em)",
+                direction: ScrollDirection::Both,
+                div {
+                    for (index, category) in children_node.clone().filter_map(|x| if x.is_category() { Some(x.value()) } else { None }).enumerate() {
+                        CustomAccordion {
+                            CategoryTree {
+                                index,
+                                category
+                            }
+                        }
+                    }
+                    for node in children_node.filter_map(|x| if x.is_setting_item() { Some(x.value()) } else { None }) {
+                        SettingEntry {
+                            node
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
